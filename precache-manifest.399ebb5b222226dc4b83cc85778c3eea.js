f288800ce9b45d66385ce4654e338fb09f0f22f5
Siddhesh Gaykar <64803476+Siddhya007@users.noreply.github.com>
2020-05-12 20:52:23 +0530

    Updates

---
+++ precache-manifest.399ebb5b222226dc4b83cc85778c3eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5418cf4a34a20503449571cf7a53e319",
+    "url": "/Siddhya007/cd20.git/index.html"
+  },
+  {
+    "revision": "fd2854cbf02564e93b03",
+    "url": "/Siddhya007/cd20.git/static/css/main.0691fb49.chunk.css"
+  },
+  {
+    "revision": "7969c663b60e658fd6ff",
+    "url": "/Siddhya007/cd20.git/static/js/2.5d91c36a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Siddhya007/cd20.git/static/js/2.5d91c36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2854cbf02564e93b03",
+    "url": "/Siddhya007/cd20.git/static/js/main.460f20e2.chunk.js"
+  },
+  {
+    "revision": "a41edb75da9ec61af2bf",
+    "url": "/Siddhya007/cd20.git/static/js/runtime-main.b479b75b.js"
+  },
+  {
+    "revision": "b33d4fb609ae6f541e26d10b084dcf0e",
+    "url": "/Siddhya007/cd20.git/static/media/image.b33d4fb6.png"
+  }
+]);
